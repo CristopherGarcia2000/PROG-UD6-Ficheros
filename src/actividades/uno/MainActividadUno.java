857a55ed753a062bcd3229274a9cfee7bb0b16c4
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-06-09 12:42:03 +0100

    feat:Primera actividad realizada

---
+++ src/actividades/uno/MainActividadUno.java
@@ -0,0 +1,36 @@
+package actividades.uno;
+
+import java.io.Console;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainActividadUno {
+    public static void main(String[] args){
+
+        Scanner console = new Scanner(System.in);
+        String texto = "";
+        while (texto.length() < 30){
+            System.out.println("Introduzca un texto de 30 caracteres o más");
+            texto = console.nextLine();
+            if (texto.length() < 30){
+                System.out.println("Te faltan "+(30-texto.length())+"palabras");
+            }
+        }
+        texto = texto.toUpperCase().replace(" ","_");
+
+        try {
+            FileWriter escritorDeTexto = new FileWriter("src/ficheros/actividad1.txt");
+            for (int i = 0; i < texto.length(); i++) {
+                escritorDeTexto.write(texto.charAt(i));
+            }
+            escritorDeTexto.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+    }
+}
