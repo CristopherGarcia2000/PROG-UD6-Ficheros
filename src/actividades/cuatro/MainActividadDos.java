744f62f4bba1c08e24c7dc551eeca3f52483dd21
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2023-06-09 13:41:03 +0100

    feat: Realizada la actividad 2

---
+++ src/actividades/cuatro/MainActividadDos.java
@@ -0,0 +1,20 @@
+package actividades.cuatro;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainActividadDos {
+    public static void main(String[] args) {
+        try {
+            FileReader lectorDePalabras = new FileReader("src/ficheros/actividad2.txt");
+            int character;
+
+            while ((character = lectorDePalabras.read()) != -1){
+                System.out.print((char)character+"_"+character+",");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
